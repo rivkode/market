f5b8994789e0e7eafbbd245fc71687c2390a96a5
jonghun <jonghuncu@gmail.com>
2024-06-21 18:12:07 +0900

    #1 test: 유저 등록 api 테스트

---
+++ src/test/java/sample/market/ControllerTestSupport.java
@@ -0,0 +1,25 @@
+package sample.market;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import sample.market.domain.user.UserService;
+import sample.market.interfaces.user.UserApiController;
+
+@WebMvcTest(controllers = {
+        UserApiController.class
+})
+public class ControllerTestSupport {
+
+    @Autowired
+    protected MockMvc mockMvc;
+
+    @Autowired
+    protected ObjectMapper objectMapper;
+
+    @MockBean
+    protected UserService userService;
+
+}
