7a5e3ccb55add85b9fa07761e428c90fe3d5738a
jonghun <jonghuncu@gmail.com>
2024-06-22 03:44:51 +0900

    #1 feat: Order DTO layer별 inner class 생성

--- src/main/java/sample/market/domain/order/OrderCommand.java
+++ src/main/java/sample/market/domain/order/OrderCommand.java
@@ -3,19 +3,22 @@ package sample.market.domain.order;
 import lombok.Builder;
 import lombok.Getter;
 
-@Getter
-@Builder
 public class OrderCommand {
-    private final Long buyerId;
-    private final Long productId;
-    private final Integer price;
 
-    public Order toEntity() {
-        return Order.builder()
-                .buyerId(buyerId)
-                .productId(productId)
-                .price(price)
-                .build();
+    @Getter
+    @Builder
+    public static class RegisterOrder {
+        private final Long buyerId;
+        private final Long productId;
+        private final Integer price;
+
+        public Order toEntity() {
+            return Order.builder()
+                    .buyerId(buyerId)
+                    .productId(productId)
+                    .price(price)
+                    .build();
+        }
     }
 
 }
