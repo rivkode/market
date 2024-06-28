547bdd9990c8ed04e52f4f24f1952d558e269f2a
jonghun <jonghuncu@gmail.com>
2024-06-28 15:32:37 +0900

    #4 test: MockBean 클래스 추가

--- src/test/java/sample/market/ControllerTestSupport.java
+++ src/test/java/sample/market/ControllerTestSupport.java
@@ -5,13 +5,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
+import sample.market.application.order.OrderFacade;
+import sample.market.application.product.ProductFacade;
+import sample.market.application.user.UserFacade;
 import sample.market.domain.user.UserService;
+import sample.market.interfaces.order.OrderApiController;
+import sample.market.interfaces.product.ProductApiController;
 import sample.market.interfaces.user.UserApiController;
 
 @WebMvcTest(controllers = {
-        UserApiController.class
+        UserApiController.class,
+        ProductApiController.class,
+        OrderApiController.class
 })
-public class ControllerTestSupport {
+public abstract class ControllerTestSupport {
 
     @Autowired
     protected MockMvc mockMvc;
@@ -21,5 +28,15 @@ public class ControllerTestSupport {
 
     @MockBean
     protected UserService userService;
+
+    @MockBean
+    protected UserFacade userFacade;
+
+    @MockBean
+    protected ProductFacade productFacade;
+
+    @MockBean
+    protected OrderFacade orderFacade;
+
 
 }
